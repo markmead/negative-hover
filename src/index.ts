ae4bfcc446fb2c42e0a19b8cae3331153d6757d5
markmead <itsmarkmead@gmail.com>
2021-01-20 13:31:02 +0000

    allow for multiple classes & fix query selector

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import { Data } from '../src/idata';
 
 export default class NegativeHover {
   parentElement: HTMLElement | null;
-  targetElements: Array<HTMLElement>;
-  inactiveClass: string;
+  targetElements!: Array<HTMLElement>;
+  inactiveClass: string[] | string;
 
   constructor(private parent: string, private data: Data) {
     this.parentElement = document.querySelector(this.parent);
-    this.targetElements = [
-      ...document.querySelectorAll(this.data.target)
-    ] as Array<HTMLElement>;
     this.inactiveClass = this.data.css;
+
+    if (this.parentElement) {
+      this.targetElements = [
+        ...this.parentElement.querySelectorAll(this.data.target)
+      ] as Array<HTMLElement>;
+    }
   }
 
   private control(): void {
@@ -29,7 +32,7 @@ export default class NegativeHover {
     if (this.targetElements && activeElement) {
       for (const targetElement of this.targetElements) {
         if (targetElement !== activeElement) {
-          targetElement.classList.add(this.inactiveClass);
+          this.addClass(targetElement);
         }
       }
     }
@@ -38,8 +41,28 @@ export default class NegativeHover {
   private remove(): void {
     if (this.targetElements) {
       for (const targetElement of this.targetElements) {
-        targetElement.classList.remove(this.inactiveClass);
+        this.removeClass(targetElement);
       }
+    }
+  }
+
+  private removeClass(currentElement: HTMLElement): void {
+    if (this.inactiveClass instanceof Array) {
+      for (const currentClass of this.inactiveClass) {
+        currentElement.classList.remove(currentClass);
+      }
+    } else {
+      currentElement.classList.remove(this.inactiveClass);
+    }
+  }
+
+  private addClass(currentElement: HTMLElement): void {
+    if (this.inactiveClass instanceof Array) {
+      for (const currentClass of this.inactiveClass) {
+        currentElement.classList.add(currentClass);
+      }
+    } else {
+      currentElement.classList.add(this.inactiveClass);
     }
   }
 
