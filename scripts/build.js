c558133f19b86ff0b8420a959c457e4461317229
Mark Mead <itsmarkmead@gmail.com>
2022-12-10 10:40:58 +0000

    Update quotes

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 buildPlugin({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: ['builds/cdn.js'],
   outfile: 'dist/hover.min.js',
 })
 
 buildPlugin({
-  entryPoints: [`builds/module.js`],
-  outfile: `dist/hover.esm.js`,
+  entryPoints: ['builds/module.js'],
+  outfile: 'dist/hover.esm.js',
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
